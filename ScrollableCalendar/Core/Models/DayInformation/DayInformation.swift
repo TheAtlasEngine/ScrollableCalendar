9a767a7d53baa2ec35271d98ea70c49009e6119a
Kosuke Nishimura <blackurocky@gmail.com>
2019-11-22 21:58:05 +0900

    implemented DayInformation protocol

--- ScrollableCalendar/Core/Models/DayInformation/DayInformation.swift
+++ ScrollableCalendar/Core/Models/DayInformation/DayInformation.swift
@@ -14,3 +14,22 @@ protocol DayInformation {
     var day: Int { get }
     var weekday: String { get }
 }
+
+struct DefaultDayInformation: DayInformation {
+    let year: Int
+    let month: Int
+    let day: Int
+    let weekday: String
+}
+
+extension DefaultDayInformation {
+    
+    init(calendar: Calendar, date: Date) {
+        self.year = calendar.component(.year, from: date)
+        self.month = calendar.component(.month, from: date)
+        self.day = calendar.component(.day, from: date)
+        
+        let weekdayIndex = calendar.component(.weekday, from: date)
+        self.weekday = calendar.shortWeekdaySymbols[weekdayIndex]
+    }
+}
