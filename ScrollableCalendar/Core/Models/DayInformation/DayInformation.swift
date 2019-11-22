39de537b267c8d4099a5c5e2bb956e8b48e1d8f3
Kosuke Nishimura <blackurocky@gmail.com>
2019-11-22 21:37:22 +0900

    added a new file

---
+++ ScrollableCalendar/Core/Models/DayInformation/DayInformation.swift
@@ -0,0 +1,13 @@
+//
+//  DayInformation.swift
+//  Core
+//
+//  Created by Kosuke Nishimura on 2019/11/22.
+//  Copyright © 2019 Kosuke Nishimura. All rights reserved.
+//
+
+import Foundation
+
+protocol DayInformation {
+    
+}
