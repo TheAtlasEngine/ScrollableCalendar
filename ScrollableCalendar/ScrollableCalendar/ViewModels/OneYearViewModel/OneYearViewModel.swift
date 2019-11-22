dab6c1f43b9895504f74ad63ab6823e5d9c3cb11
Kosuke Nishimura <blackurocky@gmail.com>
2019-11-23 08:48:53 +0900

    implemented the protocol as DefaultOneYearViewModel

--- ScrollableCalendar/ScrollableCalendar/ViewModels/OneYearViewModel/OneYearViewModel.swift
+++ ScrollableCalendar/ScrollableCalendar/ViewModels/OneYearViewModel/OneYearViewModel.swift
@@ -13,3 +13,16 @@ protocol OneYearViewModel {
     var year: Int { get }
     var isLeapYear: Bool { get }
 }
+
+struct DefaultOneYearViewModel: OneYearViewModel {
+    let year: Int
+    let isLeapYear: Bool
+}
+
+extension DefaultOneYearViewModel {
+    
+    init(yearInformation: YearInformation) {
+        self.year = yearInformation.year
+        self.isLeapYear = yearInformation.isLeapYear
+    }
+}
