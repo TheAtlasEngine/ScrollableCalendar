458e0c1f55783b753c7a20cfa5d5ab654e3c4be1
Kosuke Nishimura <blackurocky@gmail.com>
2019-11-23 16:21:40 +0900

    added a new file

---
+++ ScrollableCalendar/ScrollableCalendar/ViewModels/OneMonthCalendarViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  OneMonthCalendarViewModel.swift
+//  ScrollableCalendar
+//
+//  Created by Kosuke Nishimura on 2019/11/23.
+//  Copyright © 2019 Kosuke Nishimura. All rights reserved.
+//
+
+import Foundation
