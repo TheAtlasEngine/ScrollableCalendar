b46ba4acb2a809b9d6cf7910ac0d949b89556b4f
Kosuke Nishimura <blackurocky@gmail.com>
2019-11-23 09:13:53 +0900

    added an extension to Calendar

---
+++ ScrollableCalendar/Core/Extensions/Calendar+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Calendar+Extension.swift
+//  Core
+//
+//  Created by Kosuke Nishimura on 2019/11/23.
+//  Copyright © 2019 Kosuke Nishimura. All rights reserved.
+//
+
+import Foundation
+
+extension Calendar {
+    
+    static func calendar(locale: Locale) -> Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.locale = locale
+        return calendar
+    }
+    
+    static let japaneseCalendar = calendar(locale: .japan)
+    static let unitedStatesCalendar = calendar(locale: .unitedStates)
+}
